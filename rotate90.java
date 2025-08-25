0dac14d64c1767286736ac44290bd371a3b7cbe6
Rohini2004 <rohinikhan48@gmail.com>
2025-08-26 01:59:17 +0530

    message

---
+++ rotate90.java
@@ -0,0 +1,47 @@
+public class rotate90 {
+    public static void main(String[] args) {
+        int arr[][] = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        rotateby90(arr);
+        print(arr);
+    }
+
+    // Print the matrix
+    public static void print(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Rotate the matrix by 90 degrees clockwise
+    public static void rotateby90(int arr[][]) {
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        // Step 2: Swap columns (reverse each row)
+        int left = 0, right = arr[0].length - 1;
+        while (left < right) {
+            for (int i = 0; i < arr.length; i++) {
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+            }
+            left++;
+            right--;
+        }
+    }
+}

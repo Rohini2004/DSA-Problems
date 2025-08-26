522499a9ef76b98b23f4ac766fc122a0e5faa197
Rohini2004 <rohinikhan48@gmail.com>
2025-08-26 21:20:20 +0530

    message

---
+++ spiralmat.java
@@ -0,0 +1,47 @@
+public class spiralmat{
+    public static void main(String args[]){
+        int [][] arr= {{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+        spiralMatrix(arr);
+    }
+    public static void spiralMatrix(int [][] arr){
+        int n= arr.length;
+        int m=arr[0].length;
+        int rmin =0, rmax=arr.length-1;
+        int cmin=0, cmax=arr[0].length-1;
+        int count =0;
+        while(count<n*m){
+        //top boundary
+            for(int col =cmin ; col<=cmax ;col++)
+            {
+            System.out.print(arr[rmin][col]+" ");
+            count++;
+            }
+           rmin++;
+        //right boundary
+            for(int row =rmin ; row<=rmax ;row++)
+            {
+               System.out.print(arr[row][cmax]+" ");
+               count++;
+            }
+            cmax--;
+        //bottom boundary
+            for(int col =cmax ; col>=cmin ;col--)
+            {
+               System.out.print(arr[rmax][col]+" ");
+               count++;
+            }
+            rmax--;
+        //left boundary
+            for(int row =rmax ; row>=rmin ;row--)
+            {
+               System.out.print(arr[row][cmin]+" ");
+               count++;
+            }
+            cmin++;
+       }
+
+    }
+}
